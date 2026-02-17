183f70e13f1dbab54927145b4079165ed110dd2a
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2026-02-17 00:06:33 -0600

    Chore: converted library project from object based to class based.

--- script.js
+++ script.js
@@ -1,177 +1,188 @@
-// create an empty array
-const myLibrary = [];
-
-// create Book constructor
-function Book(id, title, author, subtitle, status) {
-    this.id = id;
-    this.author = author;
-    this.title = title;
-    this.subtitle = subtitle;
-    this.status = status;
-}
-
-// take params, create a book then store it in the array
-// NOTE: now takes (title, author, subtitle)
-function addBookToLibrary(title, author, subtitle, status) {
-    let id = crypto.randomUUID();
-    let newBook = new Book(id, title, author, subtitle, status);
-    myLibrary.push(newBook);
-}
-
-// sample books for testing purposes
-addBookToLibrary("The Fellowship of the Ring", "J.R.R. Tolkien", "Being the First Part of The Lord of the Rings", "Read");
-
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "A Novel", "Read");
-
-addBookToLibrary("1984", "George Orwell", "A Dystopian Novel", "Unread");
-
-addBookToLibrary("Frankenstein", "Mary Shelley", "Or, The Modern Prometheus", "Unread");
-
-addBookToLibrary("Pride and Prejudice", "Jane Austen", "A Story of Love and Misunderstanding", "Unread");
-
-addBookToLibrary("Foundation", "Isaac Asimov", "The Saga of the Psychohistorians", "Unread");
-
-addBookToLibrary("A Wizard of Earthsea", "Ursula K. Le Guin", "The First Book of Earthsea", "Read");
-
-addBookToLibrary("Kindred", "Octavia E. Butler", "A Novel", "Read");
-
-addBookToLibrary("Beloved", "Toni Morrison", "A Novel", "Read");
-
-addBookToLibrary("Mistborn", "Brandon Sanderson", "The Final Empire", "Unread");
-
-addBookToLibrary("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "Don't Panic", "Read");
-
-addBookToLibrary("Dune", "Frank Herbert", "A Heroic Saga on the Desert Planet Arrakis", "Read");
-
-
-
-/** DISPLAY BOOKS SCRIPT */
-// grab the container
+// Global Book Container
 let bookContainer = document.querySelector(".books__container");
 
-// display the books
-function displayBooks(myLibrary) {
-    // clear the container since this is a reusable function
+// Create Book Class
+class Book {
+    // Create Book constructor
+    constructor(title, author, subtitle, status = "Unread") {
+        this.id = crypto.randomUUID();
+        this.author = author;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.status = status;
+    }
+}
+
+// Create Library Class
+class Library {
+    // Fields
+    library;
+
+    constructor(library = []) {
+        if (!Array.isArray(library)) {
+            throw new Error("Library constructor expects an array.")
+        }
+        this.library = library;
+    }
+
+    addBookToLibrary(book) {
+        this.library.push(book);
+    }
+
+    changeBookStatus(bookIndex) {
+        for (let i = 0; i < this.library.length; i++) {
+            if (this.library[i].id === bookIndex) {
+                this.library[i].status === "Read" ? this.library[i].status = "Unread" : this.library[i].status = "Read";
+                break;
+            }
+        }
+    }
+
+    deleteBookFromLibrary(bookIndex) {
+        for (let i = 0; i < this.library.length; i++) {
+            if (this.library[i].id === bookIndex) {
+                this.library.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+// Create a new library and add all sample books to the library
+const myLibrary = new Library();
+
+// Sample books for testing purposes
+myLibrary.addBookToLibrary(new Book("The Fellowship of the Ring", "J.R.R. Tolkien", "Being the First Part of The Lord of the Rings", "Read"));
+
+myLibrary.addBookToLibrary(new Book("To Kill a Mockingbird", "Harper Lee", "A Novel", "Read"));
+
+myLibrary.addBookToLibrary(new Book("1984", "George Orwell", "A Dystopian Novel", "Unread"));
+
+myLibrary.addBookToLibrary(new Book("Frankenstein", "Mary Shelley", "Or, The Modern Prometheus", "Unread"));
+
+myLibrary.addBookToLibrary(new Book("Pride and Prejudice", "Jane Austen", "A Story of Love and Misunderstanding", "Unread"));
+
+myLibrary.addBookToLibrary(new Book("Foundation", "Isaac Asimov", "The Saga of the Psychohistorians", "Unread"));
+
+myLibrary.addBookToLibrary(new Book("A Wizard of Earthsea", "Ursula K. Le Guin", "The First Book of Earthsea", "Read"));
+
+myLibrary.addBookToLibrary(new Book("Kindred", "Octavia E. Butler", "A Novel", "Read"));
+
+myLibrary.addBookToLibrary(new Book("Beloved", "Toni Morrison", "A Novel", "Read"));
+
+myLibrary.addBookToLibrary(new Book("Mistborn", "Brandon Sanderson", "The Final Empire", "Unread"));
+
+myLibrary.addBookToLibrary(new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "Don't Panic", "Read"));
+
+myLibrary.addBookToLibrary(new Book("Dune", "Frank Herbert", "A Heroic Saga on the Desert Planet Arrakis", "Read"));
+
+// --------------------BUILD AND DISPLAY BOOKS----------------//
+const displayBooks = (myLibrary) => {
+    // Clear the container since this is a reusable function
     bookContainer.innerHTML = "";
 
-    // get the library size
-    let size = myLibrary.length;
+    // Get the size of the library property in the myLibrary object
+    let size = myLibrary.library.length;
 
-    // loop through the array
+    // Loop through the array, build the book elements and append to the book container
     for (let i = 0; i < size; i++) {
-        // build book elements
-        // build the article to hold books
+        // ----------------- BOOK ELEMENTS ----------------- //
+        // Build the article to hold book elements
         const book = document.createElement("article");
         book.classList.add("book");
+        book.setAttribute("data-index", myLibrary.library[i].id);
 
-        // give the book a data attribute
-        book.setAttribute("data-index", myLibrary[i].id);
-
-        // build the book title
+        // Build the elements for the book title
         const title = document.createElement("p");
         title.classList.add("title");
-        title.textContent = myLibrary[i].title;
+        title.textContent = myLibrary.library[i].title;
 
-        // build the author
+        // Build the elements for the book author
         const author = document.createElement("p");
         author.classList.add("author");
-        author.textContent = myLibrary[i].author;
+        author.textContent = myLibrary.library[i].author;
 
-        // build the subtitle
+        // Build the elements for the book subtitle
         const subtitle = document.createElement("p");
         subtitle.classList.add("subtitle");
-        subtitle.textContent = myLibrary[i].subtitle;
+        subtitle.textContent = myLibrary.library[i].subtitle;
 
-        // build the status
+        // Build the elements for the book status
         const status = document.createElement("p");
         status.classList.add("status");
-        status.textContent = `Status: ${myLibrary[i].status}`;
+        status.textContent = `Status: ${myLibrary.library[i].status}`;
 
-        // add button subsection to the container
+        // --------------------BOOK BUTTONS ----------------//
+        // Add a container to hold buttons for each book
         const buttonContainer = document.createElement("div");
         buttonContainer.classList.add("button-container");
 
-        // add a button to the book
+        // Add a delete button
         const deleteButton = document.createElement("button");
         deleteButton.classList.add("deleteButton");
         deleteButton.textContent = "Delete"
 
-        // add a button to the book
+        // Add a change status button
         const statusButton = document.createElement("button");
         statusButton.classList.add("statusButton");
         statusButton.textContent = "Change Status"
 
-        // build the book and add to the container
-        book.append(title);
-        book.append(author);
-        book.append(subtitle);
-        book.append(status);
+        // Add book elements to the book
+        book.append(title, author, subtitle, status);
 
-        // append the container to the book
-        buttonContainer.append(deleteButton);
-        buttonContainer.append(statusButton)
+        // Append buttons to the buttton container
+        buttonContainer.append(deleteButton, statusButton);
+
+        // Append the buttonContainer to the book
         book.append(buttonContainer);
 
-        // finally add the book to our bookContainer
+        // Finally append the book to the bookContainer
         bookContainer.append(book);
     }
 }
 
+// Display the books
 displayBooks(myLibrary);
 
-/** ADD NEW BOOKS SCRIPT */
-// grab the add new book button
+// --------------------ADD A NEW BOOK TO THE LIBRARY--------------------//
+// Grab the add new book button
 const newBookButton = document.querySelector(".header__actions-button");
 
-// grab the modal dialog
+// Grab the confirm addition of new book button
+const newBookConfirmButton = document.querySelector(".dialog__button-confirm");
+
+// Grab the cancel addition of new book button
+const newBookCancelButton = document.querySelector(".dialog__button-cancel");
+
+// Grab the modal dialog
 const newBookDialog = document.querySelector(".dialog");
 
-// user clicks the button so I want the modal to open
+// Grab the form
+const form = document.querySelector(".dialog__form");
+
+// Opens the modal on user click
+// Note: We don't need a cancel listener because of how we've set up our cancel button
 newBookButton.addEventListener("click", () => {
     newBookDialog.showModal();
 });
 
-// Note: We don't need a cancel listener because of how we've set up our cancel button
-
-/** BOOK SUBMISSION LOGIC **/
-// CANCELLATION PHASE
-const newBookCancelButton = document.querySelector(".dialog__button-cancel");
-
+// Cancels the book addition on click
 newBookCancelButton.addEventListener("click", () => {
     // just close
     newBookDialog.close();
 });
 
-// CONFIRM/SUBMISSION PHASE
-const form = document.querySelector(".dialog__form");
-
-form.addEventListener("submit", (e) => {
-    // this listener ensures form validation occurs 
-    // prevent default behavior
-    e.preventDefault();
-});
-
-const newBookConfirmButton = document.querySelector(".dialog__button-confirm");
-
-newBookConfirmButton.addEventListener("click", () => {
-    // after form submission validation is enforced
-    // this listener handles actual valid submissions
-    // close the dialog with a non default value
-    // don't reset already input values if the user cancels or escapes out
-    newBookDialog.close("New Book Added");
-});
-
 // Attach a listener to our modal that reacts on close
 newBookDialog.addEventListener("close", () => {
 
-    if (newBookDialog.returnValue === "New Book Added") {
+    if (newBookDialog.returnValue === "default") {
         // grab each value of the book elements
         const newBookTitle = document.querySelector("#title");
         const newBookAuthor = document.querySelector("#author");
         const newBookSubtitle = document.querySelector("#subtitle");
         const newBookStatus = document.querySelector("#status")
         // add new book to library
-        addBookToLibrary(newBookTitle.value, newBookAuthor.value, newBookSubtitle.value, newBookStatus.value);
+        myLibrary.addBookToLibrary(new Book(newBookTitle.value, newBookAuthor.value, newBookSubtitle.value, newBookStatus.value));
         // reset values for next valid run
         // reset everything
         newBookTitle.value = "";
@@ -183,64 +194,39 @@ newBookDialog.addEventListener("close", () => {
     }
 });
 
-
-/** DELETE LOGIC **/
-// We will attach a listener to the parent since we have too many buttons
+// --------------------DELETE AN EXISTING BOOK FROM THE LIBRARY--------------------//
+// Attach a listener to the parent since we have too many buttons
 bookContainer.addEventListener("click", (e) => {
     // If it does have the deleteButton class
     if (e.target.classList.contains("deleteButton")) {
-        // grab the needed book element and index
+        // Grab the book element and index
         const bookElement = e.target.closest(".book");
         const bookIndex = bookElement.dataset.index;
-        // then delete the book
-        deleteBook(bookIndex);
+        // Then delete the book from the array
+        myLibrary.deleteBookFromLibrary(bookIndex);
+        // Redraw the UI
+        displayBooks(myLibrary);
     }
     else {
-        // return if we don't have the class we want
+        // Return if we don't have the class we want
         return;
     }
 });
 
-function deleteBook(bookIndex) {
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].id === bookIndex) {
-            myLibrary.splice(i, 1);
-            break;
-        }
-    }
-    displayBooks(myLibrary);
-}
-
-/** CHANGE STATUS LOGIC **/
-// We will attach a listener to the parent since we have too many buttons
+// --------------------CHANGE THE STATUS OF AN EXISTING BOOK IN THE LIBRARY--------------------//
 bookContainer.addEventListener("click", (e) => {
-    // If it does have the deleteButton class
+    // If it does have the change status class
     if (e.target.classList.contains("statusButton")) {
-        // grab the needed book element and index
+        // Grab the book element and index
         const bookElement = e.target.closest(".book");
         const bookIndex = bookElement.dataset.index;
-        // then delete the book
-        newStatus(bookIndex);
+        // Then change the status of the book
+        myLibrary.changeBookStatus(bookIndex);
+        // Redraw the UI
+        displayBooks(myLibrary);
     }
     else {
-        // return if we don't have the class we want
+        // Return if we don't have the class we want
         return;
     }
 });
-
-Book.prototype.changeStatus = function () {
-    this.status === "Read" ? this.status = "Unread" : this.status = "Read";
-}
-
-// it is important that we change the status in the array and redraw
-// rather than just change the status on the html
-// this way we maintain data integrity
-function newStatus(bookIndex) {
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].id === bookIndex) {
-            myLibrary[i].changeStatus();
-            break;
-        }
-    }
-    displayBooks(myLibrary);
-}
